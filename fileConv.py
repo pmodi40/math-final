42e5cd8ccb4e4996b8a119eaa2cf70286ffc29b7
pmodi40 <124059345+pmodi40@users.noreply.github.com>
2024-05-17 12:30:23 -0400

    Update fileConv.py

--- fileConv.py
+++ fileConv.py
@@ -5,7 +5,11 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'pretty_midi'])
 
 import pretty_midi
 
+# Create fields and constructor!
+
 def genData(midiFile):
+  midi_data = pretty_midi.PrettyMidi(midiFile)
+  
   # Made to unpack the following data points into a list of lists:
   # Note, Velocity, Tempo, Time
   print("Stub")
