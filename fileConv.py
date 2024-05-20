ef334dc4fb9df782538f4a67a198668c95414e80
pmodi40 <124059345+pmodi40@users.noreply.github.com>
2024-05-20 12:32:40 -0400

    Update fileConv.py

--- fileConv.py
+++ fileConv.py
@@ -7,11 +7,36 @@ import pretty_midi
 
 # Create fields and constructor!
 
+noteList = []
+
+def __init__(self, file):
+  # Could always just pre-build the notes you take, exclude the rest for standardization
+  fullArr = genData(file)
+  allNotes = listDubInd(fullArr, 0)
+  for j in allNotes:
+    if j in listDubInd(allNotes, 0):
+      
+    else:
+        
+
 def genData(midiFile):
   midi_data = pretty_midi.PrettyMidi(midiFile)
-  
+  tempo = midi_data.estimate_tempo()
+  notesCol = []
+  """
+  General Structure:
+  [Note *Name*, Note *Number*, Velocity, Duration, ]
+  """
+  for i in midi_data.instruments:
+    for j in i.notes:
+      notesCol.append([pretty_midi.note_number_to_name(j.pitch), j.pitch, j.velocity, j.get_duration(), i.name])
   # Made to unpack the following data points into a list of lists:
   # Note, Velocity, Tempo, Time
-  print("Stub")
+  return notesCol
 
+def listDubInd(list, ind):
+  arr = []
+  for i in list:
+    arr.append(i[ind])
+  return arr
   
