d77173537c28deb00d11cf1e9861ec62668a0f21
pmodi40 <124059345+pmodi40@users.noreply.github.com>
2024-05-21 12:09:05 -0400

    Update fileConv.py

--- fileConv.py
+++ fileConv.py
@@ -7,17 +7,17 @@ import pretty_midi
 
 # Create fields and constructor!
 
-noteList = []
-
+markov = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+NotesList = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+"""
 def __init__(self, file):
   # Could always just pre-build the notes you take, exclude the rest for standardization
   fullArr = genData(file)
   allNotes = listDubInd(fullArr, 0)
-  for j in allNotes:
-    if j in listDubInd(allNotes, 0):
+  
+  for i in allNotes
+"""
       
-    else:
-        
 
 def genData(midiFile):
   midi_data = pretty_midi.PrettyMidi(midiFile)
@@ -25,18 +25,20 @@ def genData(midiFile):
   notesCol = []
   """
   General Structure:
-  [Note *Name*, Note *Number*, Velocity, Duration, ]
+  [Note *Name*, Note *Number*, Velocity, Duration, Instrument]
   """
   for i in midi_data.instruments:
     for j in i.notes:
-      notesCol.append([pretty_midi.note_number_to_name(j.pitch), j.pitch, j.velocity, j.get_duration(), i.name])
+      print(pretty_midi.note_number_to_name(j.pitch))
+      #notesCol.append([pretty_midi.note_number_to_name(j.pitch), j.pitch, j.velocity, j.get_duration(), i.name])
   # Made to unpack the following data points into a list of lists:
   # Note, Velocity, Tempo, Time
-  return notesCol
+  # return notesCol
 
 def listDubInd(list, ind):
   arr = []
   for i in list:
     arr.append(i[ind])
   return arr
-  
+
+genData("");
