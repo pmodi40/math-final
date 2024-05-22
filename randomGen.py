022757a6a7aa134d213c8df0c133ed4f9f084768
pmodi40 <124059345+pmodi40@users.noreply.github.com>
2024-05-22 11:43:51 -0400

    Update randomGen.py

--- randomGen.py
+++ randomGen.py
@@ -1,20 +1,4 @@
 import fileConv
 from os import walk
 
-def collapse(dubList):
-    music = []
-    for i in dubList:
-        adder = i[0]
-        for j in range(1, len(i)):
-            music.append(adder + "/" + i[j])
-    return music
-
-musicList = []
-for (dir, dir_name, file) in walk("music"):
-    musicList.append([dir] + file)
-musicList = collapse(musicList)
-
-
-
-hi = fileConv.markov(musicList)[0]
-print(hi)
+fileConv.createMusic("output.mid", "C#4")
