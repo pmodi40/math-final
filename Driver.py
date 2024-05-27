cb88738ef23d8ea4a57520d3439b78d03c6564c7
pmodi40 <pmodi40@stuy.edu>
2024-05-27 17:07:04 -0400

    Added a README

--- Driver.py
+++ Driver.py
@@ -3,7 +3,7 @@ import musicGen, sys
 mode = sys.argv[1]
 fileName = sys.argv[2]
 
-if mode == "S":
+if mode == "F":
     musicGen.createNormMusic(fileName, sys.argv[3])
-elif mode == "D":
+elif mode == "S":
     musicGen.createChordMusic(fileName, sys.argv[3], sys.argv[4])
